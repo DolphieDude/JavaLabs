6f93b1080d0eb16a8cabf200a658e59d7aa08141
DolphieDude <andreycherevat@gmail.com>
2023-09-24 22:15:19 +0200

    add task1_15

---
+++ src/lab1/Task1_15.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class Task1_15 {
+    public static void main(String[] args) {
+        double side, angle;
+        try {
+            side = Math.abs(Double.parseDouble(args[0]));
+            angle = Math.abs(Double.parseDouble(args[1]));
+        }
+        catch (Exception e) {
+            System.out.println("Not doubles");
+            return;
+        }
+
+        if (angle >= 180.) {
+            System.out.println("Rhombus is not valid");
+            return;
+        }
+
+        double area = Math.pow(side, 2.) * Math.sin(Math.toRadians(angle));
+
+        System.out.printf("Area is %.1f\n", area);
+    }
+}

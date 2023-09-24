7afa43b18016ae4f62c183f9ae21cfb97a38bcd7
DolphieDude <andreycherevat@gmail.com>
2023-09-24 21:41:41 +0200

    added checks to Task1_5

--- src/lab1/Task1_5.java
+++ src/lab1/Task1_5.java
@@ -2,16 +2,21 @@ package lab1;
 public class Task1_5 {
     public static void main(String[] args) {
         double a, b, c;
-        a = Double.parseDouble(args[0]);
-        b = Double.parseDouble(args[1]);
-        c = Double.parseDouble(args[2]);
+        a = Math.abs(Double.parseDouble(args[0]));
+        b = Math.abs(Double.parseDouble(args[1]));
+        c = Math.abs(Double.parseDouble(args[2]));
+
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            System.out.println("Triangle is not valid");
+            return;
+        }
 
         double semiPerimeter = (a + b + c) / 2.;
 
         double area =   Math.sqrt(semiPerimeter * (semiPerimeter - a) *
                 (semiPerimeter - b) * (semiPerimeter - c));
 
-        System.out.println("Area is " + area);
-        System.out.println("Semi Perimeter is " + semiPerimeter);
+        System.out.printf("Area is %.1f\n", area);
+        System.out.printf("Semi Perimeter is %.1f\n", semiPerimeter);
     }
 }

26f88bcc541694b45f170f31a12f08dfa2ac07ee
DolphieDude <andreycherevat@gmail.com>
2023-10-11 10:36:50 +0200

    add Task2_25

---
+++ src/lab2/Task2_25.java
@@ -0,0 +1,46 @@
+package lab2;
+
+import java.util.HashMap;
+
+public class Task2_25 {
+    public static void main(String[] args) {
+        int dayOfMonth;
+        int month;
+        boolean isLeapYear;
+        try {
+            dayOfMonth = Integer.parseInt(args[0]);
+            month = Integer.parseInt(args[1]);
+            if (month > 12) throw new Exception();
+            isLeapYear = Boolean.parseBoolean(args[2]);
+        } catch (Exception e) {
+            System.out.println("Wrong arguments.");
+            return;
+        }
+
+        int additionalDay = isLeapYear ? 1 : 0;
+        HashMap<Integer, Integer> mapOfMonths = new HashMap<>();
+        mapOfMonths.put(1, 0);
+        mapOfMonths.put(2, 31);
+        mapOfMonths.put(3, 59 + additionalDay);
+        mapOfMonths.put(4, 90 + additionalDay);
+        mapOfMonths.put(5, 120 + additionalDay);
+        mapOfMonths.put(6, 151 + additionalDay);
+        mapOfMonths.put(7, 181 + additionalDay);
+        mapOfMonths.put(8, 212 + additionalDay);
+        mapOfMonths.put(9, 243 + additionalDay);
+        mapOfMonths.put(10, 273 + additionalDay);
+        mapOfMonths.put(11, 314 + additionalDay);
+        mapOfMonths.put(12, 334 + additionalDay);
+        mapOfMonths.put(13, 365 + additionalDay);
+
+        int dayOfYear;
+        dayOfYear = dayOfMonth + mapOfMonths.get(month);
+        if (dayOfYear > mapOfMonths.get(month + 1)) {
+            System.out.println("Day is not real for this month");
+            return;
+        }
+
+
+        System.out.println("Day of the year: " + dayOfYear);
+    }
+}
